149cffdc48df36a8cfc2bec6741a05af6f2a5f9a
TimCheersNote <artem1978354@gmail.com>
2023-12-21 13:02:37 +0500

    Добавил модель БД, модель показателей внутри приложения

---
+++ Project/Report-system-model/Report-system-model/Models/DBModels/DataStatus.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Report_system_model.DBModels;
+
+[Table ("data_status")]
+public class DataStatus
+{
+    [Key] [MaxLength(25)] public string Value { get; set; }
+}
