d277fa7f716b1470da26d2309e7b11447c75e9c7
osheat <oleg9600@gmail.com>
2023-12-21 12:43:24 +0500

    init commit

---
+++ Project/Report-system-model/Report-system-model/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Report_system_model.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
