f109f349e94965dad44b443511c4470fb4f4b086
TimCheersNote <artem1978354@gmail.com>
2023-12-21 13:09:31 +0500

    изменил context

--- Project/Report-system-model/Report-system-model/Models/MyDbContext.cs
+++ Project/Report-system-model/Report-system-model/Models/MyDbContext.cs
@@ -29,7 +29,7 @@ public class MyDbContext : DbContext
         {
             Host = "localhost",
             Port = 5432,
-            Database = "keyfigureDB",
+            Database = "rsmDB",
             Username = "postgres",
             Password = "123321",
         };
