a3686029359f95968ab4f11af8b02057f7946d9c
TimCheersNote <artem1978354@gmail.com>
2023-12-21 17:56:41 +0500

    изменил формы для показателей (неолные)

--- Project/Report-system-model/Report-system-model/ViewModels/MainWindowViewModel.cs
+++ Project/Report-system-model/Report-system-model/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Media;
 using Microsoft.EntityFrameworkCore;
 using ReactiveUI;
@@ -13,13 +15,29 @@ namespace Report_system_model.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    [Reactive] public int showFullName { get; set; }
+    [Reactive] public int showShortName { get; set; }
+    [Reactive] public int showDataStatus { get; set; }
+    [Reactive] public ObservableCollection<KeyfigureModel> keyfigureModels { get; set; }
+    public ReactiveCommand<Unit, Unit> IsFullNameCheckBoxChecked { get; }
+    [Reactive] public bool FullNemeCheck { get; set; }
     public MainWindowViewModel()
-    {   
+    {
         MyDbContext db = new MyDbContext();
         db.Database.EnsureCreated();
         KeyfigureModel tmp = new KeyfigureModel();
         keyfigureModels = new ObservableCollection<KeyfigureModel>(tmp.GetCompleteInformation().ToList());
+        IsFullNameCheckBoxChecked= ReactiveCommand.Create(() =>
+        {
+            if (FullNemeCheck == true)
+            {
+                showFullName = 200;
+                
+            }
+            else
+            {
+                showFullName = 0;
+            }
+        });
     }
-
-    [Reactive] public ObservableCollection<KeyfigureModel> keyfigureModels { get; set; }
 }
--- Project/Report-system-model/Report-system-model/Views/MainWindow.axaml.cs
+++ Project/Report-system-model/Report-system-model/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Report_system_model.Views;
 
@@ -8,4 +9,5 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
 }
