cb5ac37f463fdb2f0b200cd785bb24a0656a4012
VK <vadimkarelov02@gmail.com>
2024-01-31 15:57:02 +0300

    GELO

---
+++ Report-system-model/Models/DBModels/Company.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Report_system_model.DBModels;
+
+[Table ("company")]
+public class Company
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int id { get; set; }
+    [MaxLength(500)] public string title { get; set; }
+}
