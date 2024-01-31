282b64d10bcfdd0ea0c5658a6b50fb0284ca4cf9
VK <vadimkarelov02@gmail.com>
2024-01-31 16:01:37 +0300

    OLEG

---
+++ Report-system-model/Models/DBModels/ReportUsageIndicator.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Report_system_model.DBModels;
+
+[Table("report_usage_indicator")]
+public class ReportUsageIndicator
+{
+    [Key] [MaxLength(50)] public string value { get; set; }
+}
---
+++ Report-system-model/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Report_system_model.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
