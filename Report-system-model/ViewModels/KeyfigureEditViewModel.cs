1b641f06f5056d7d1571cf6bcc2a33e20fc44efd
VK <vadimkarelov02@gmail.com>
2024-01-31 16:42:33 +0300

    УЧЕБА

--- Report-system-model/ViewModels/KeyfigureEditViewModel.cs
+++ Report-system-model/ViewModels/KeyfigureEditViewModel.cs
@@ -24,6 +24,13 @@ namespace Report_system_model.ViewModels;
 public class KeyfigureEditViewModel
 {
     [Reactive] public KeyfigureModel currModel { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public KeyfigureEditViewModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+    }
+
     public KeyfigureEditViewModel(KeyfigureModel selectedModel)
     {
         currModel = selectedModel;
--- Report-system-model/ViewModels/MainWindowViewModel.cs
+++ Report-system-model/ViewModels/MainWindowViewModel.cs
@@ -28,6 +28,9 @@ public class MainWindowViewModel : ViewModelBase
     [Reactive] public string searchString { get; set; }
     private List<KeyfigureModel> keyList;
     public ReactiveCommand<Unit, Unit> ButtonClickCommand { get; private set; }
+    
+    public ReactiveCommand<KeyfigureModel, Unit> ButtonClickCommand_1 { get; private set; }
+    
     public MainWindowViewModel()
     {
         MyDbContext db = new MyDbContext();
@@ -39,6 +42,15 @@ public class MainWindowViewModel : ViewModelBase
         keyfigureModels = new ObservableCollection<KeyfigureModel>(keyList);
 
         ButtonClickCommand = ReactiveCommand.Create(OpenNewWindowButton_Click);
+        ButtonClickCommand_1 = ReactiveCommand.Create<KeyfigureModel, Unit>(Execute);
+    }
+
+    private Unit Execute(KeyfigureModel obj)
+    {
+        KeyfigureEditWindow newWindow = new KeyfigureEditWindow();
+        newWindow.Show();                                         
+        
+        return Unit.Default;
     }
     private void OpenNewWindowButton_Click()    
     {
