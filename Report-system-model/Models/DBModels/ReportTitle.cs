fd214d0f36a522a7a56a336483c16d3e1b29864b
Tim <73362594+TimCheers@users.noreply.github.com>
2024-02-05 14:47:21 +0500

    Обновил создание БД через ORM (по изменениям Галинова О. Ю.)
    (cherry picked from commit fd675a56001e8d31548d864079064733ccb9ab19)

---
+++ Report-system-model/Models/DBModels/ReportTitle.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Report_system_model.DBModels;
+
+[Table("report_title")]
+public class ReportTitle
+{
+    [Key] [MaxLength(100)] public string value { get; set; }
+}
