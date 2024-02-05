fd675a56001e8d31548d864079064733ccb9ab19
Tim <73362594+TimCheers@users.noreply.github.com>
2024-02-05 14:47:21 +0500

    Обновил создание БД через ORM (по изменениям Галинова О. Ю.)

--- Report-system-model/Models/MyDbContext.cs
+++ Report-system-model/Models/MyDbContext.cs
@@ -21,6 +21,14 @@ public class MyDbContext : DbContext
     public DbSet<SourceSystem> SourceSystems { get; set; }
     public DbSet<UploadDeadline> UploadDeadlines { get; set; }
     public DbSet<ValueType> ValueTypes { get; set; }
+    public DbSet<AnalyticalFeature> AnalyticalFeatures { get; set; }
+    public DbSet<BusinessProcess> BusinessProcesses { get; set; }
+    public DbSet<FormationFrequency> FormationFrequencies { get; set; }
+    public DbSet<Report> Reports { get; set; }
+    public DbSet<ReportCode> ReportCodes { get; set; }
+    public DbSet<ReportId> ReportIds { get; set; }
+    public DbSet<ReportIndicator> ReportIndicators { get; set; }
+    public DbSet<ReportTitle> ReportTitles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -28,7 +36,7 @@ public class MyDbContext : DbContext
         {
             Host = "localhost",
             Port = 5432,
-            Database = "rsm5",
+            Database = "rsm7",
             Username = "postgres",
             Password = "postgres",
         };
