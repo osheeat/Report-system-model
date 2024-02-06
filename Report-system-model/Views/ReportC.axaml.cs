cbf914ac85f98dacc9511ab1e9fa5e906b8a17b1
Tim <73362594+TimCheers@users.noreply.github.com>
2024-02-06 14:49:09 +0500

    Разделил главное окно на UserControl

---
+++ Report-system-model/Views/ReportC.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Report_system_model.Views;
+
+public partial class ReportC : UserControl
+{
+    public ReportC()
+    {
+        InitializeComponent();
+    }
+}
