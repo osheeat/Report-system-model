f84a066ace125d0a6359bcd7a6f4f960b1e87d6a
osheat <oleg9600@gmail.com>
2024-02-12 12:01:01 +0500

    Создал первую версию формы для просмотра отчетов на основе формы для показателей

---
+++ Report-system-model/ViewModels/ReportCVM.cs
@@ -0,0 +1,29 @@
+﻿using Report_system_model.AppModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reactive;
+using System.Threading;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using SkiaSharp;
+using Report_system_model.Views;
+
+namespace Report_system_model.ViewModels;
+
+public class ReportCVM : ViewModelBase
+{
+    [Reactive] public ObservableCollection<KeyfigureModel> keyfigureModels { get; set; }
+    [Reactive] KeyfigureModel SelectedKeyfigureModel { get; set; }
+    [Reactive] public string searchString { get; set; }
+    public ReactiveCommand<KeyfigureModel, Unit> ButtonClickCommand_1 { get; private set; }
+}
